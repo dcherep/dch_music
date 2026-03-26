cddf20628ee0d05d40f1f193c7182d15717c3ed3
Darya <dch@gmail.ru>
2026-03-26 09:17:31 +0700

    добавлена модель исполнителя

--- music/main/models.py
+++ music/main/models.py
@@ -6,10 +6,20 @@ class Genre(models.Model):
     description=models.CharField(max_length=5000)
     def __str__(self):
         return self.name_ru
+    
+class Artist(models.Model):
+    name=models.CharField(max_length=500, unique=True)
+    image=models.ImageField(upload_to='artists/',blank=True,null=True)
+
+    def __str__(self):
+        return self.name
 
 class Track(models.Model):
     title=models.CharField(max_length=500, unique=True)
-    duration=models.IntegerField()
+    duration=models.IntegerField(help_text="Длительность в секундах")
     genres=models.ManyToManyField(Genre)
+    artist=models.ForeignKey(Artist, on_delete=models.RESTRICT,null=True)
+    audio_file=models.FileField(upload_to='music/tracks/',blank=True,null=True)
+    
     def __str__(self):
         return self.title
